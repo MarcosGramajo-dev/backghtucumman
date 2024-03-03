28b84c9ad3aa657a6511b9d3719eac2a57d19825
Marcos <marcosgramajo96@gmail.com>
2024-03-03 17:34:09 -0300

    add feedback url

--- functions/server.js
+++ functions/server.js
@@ -8,76 +8,112 @@ import fs from 'fs';
 const app = express();
 
 const corsOptions = {
-    origin: '*', // Permitir solicitudes desde este origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-    credentials: true, // Permitir enviar cookies y encabezados de autenticación
-  };
-  
+  origin: 'https://ghtucuman.com.ar',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
-  
-const router = express.Router();
 app.use(cors(corsOptions));
+app.use(express.json());
+
+const router = express.Router();
 
 mercadopago.configure({
-	access_token: "TEST-6228624431860766-022718-6803ca6d10fd708ebcc008b0e465b7b7-1243177028",
+  access_token: "TEST-6228624431860766-022718-6803ca6d10fd708ebcc008b0e465b7b7-1243177028",
 });
 
 router.get('/', (req, res) => {
-    const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>HTML Rendering</title>
-        </head>
-        <body>
-            <h1>Hello, this is rendered HTML!</h1>
-        </body>
-        </html>
-    `;
-    
-    res.send(htmlContent);
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HTML Rendering</title>
+    </head>
+    <body>
+        <h1>Hello, this is rendered HTML!</h1>
+    </body>
+    </html>
+  `;
+
+  res.send(htmlContent);
 });
 
 router.post("/create_preference", (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity)
+      }
+    ],
+    back_urls: {
+      "success": "https://ghtucuman.com.ar/feedback",
+      "failure": "https://ghtucuman.com.ar/feedback",
+      "pending": "https://ghtucuman.com.ar/feedback"
+    },
+    auto_return: "approved",
+  };
 
-	let preference = {
-		items: [
-			{
-				title: req.body.description,
-				unit_price: Number(req.body.price),
-				quantity: Number(req.body.quantity),
-				external_reference: Number(req.body.id)
-			}
-		],
-		back_urls: {
-			"success": "http://localhost:8080/feedback",
-			"failure": "http://localhost:3000/feedback",
-			"pending": "http://localhost:3000/feedback"
-		},
-		auto_return: "approved",
-	};
-
-	mercadopago.preferences.create(preference)
-		.then(function (response) {
-			res.json({
-				id: response.body.id
-			});
-		}).catch(function (error) {
-			console.log(error);
-		});
+  mercadopago.preferences.create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id
+      });
+    }).catch(function (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
 router.get('/participants', function (req, res) {
-    const { id } = req.query;
+  const { id } = req.query;
 
-    if (id) {
-        const participant = participants.find(participant => participant.id === parseInt(id, 10));
+  if (id) {
+    const participant = participants.find(participant => participant.id === parseInt(id, 10));
 
-        if (participant) {
-            res.json({
-                participant
+    if (participant) {
+      res.json({
+        participant
+      });
+    } else {
+      res.status(404).json({
+        error: 'Participant not found'
+      });
+    }
+  } else {
+    res.json({
+      participants
+    });
+  }
+});
+
+
+router.get('/feedback', function (req, res) {
+    if (req.query.status === 'approved') {
+        // Busca el participante por su id
+        const participantIndex = participants.findIndex(participant => participant.id === participant_id);
+
+        if (participantIndex !== -1) {
+            // Actualiza la cantidad de votos del participante
+            participants[participantIndex].quantity_votes += quantity_votes;
+
+            // Escribe los cambios de vuelta al archivo
+            fs.writeFile('./db/participants.json', JSON.stringify(participants), 'utf-8', (err) => {
+                if (err) {
+                    console.error('Error writing participants file:', err);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                } else {
+                    // Envía la respuesta con la información del pago y el participante actualizado
+                    res.json({
+                        Payment: req.query.payment_id,
+                        Status: req.query.status,
+                        MerchantOrder: req.query.merchant_order_id,
+                        UpdatedParticipant: participants[participantIndex]
+                    });
+                }
             });
         } else {
             res.status(404).json({
@@ -86,14 +122,14 @@ router.get('/participants', function (req, res) {
         }
     } else {
         res.json({
-            participants
+            Payment: req.query.payment_id,
+            Status: req.query.status,
+            MerchantOrder: req.query.merchant_order_id,
         });
     }
 });
 
-app.use('/.netlify/functions/server', router);
-export const handler = serverless(app);
 
-// app.listen(8080, () => {
-// 	console.log("The server is now running on Port 8080");
-// });
+app.use('/.netlify/functions/server', router);
+
+export const handler = serverless(app);
